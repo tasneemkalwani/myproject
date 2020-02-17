6f2a06cbfd5251acce9a370c8e45cdfda37c77b9
sakina12344 <tazeenfatima2801@gmail.com>
2020-02-17 13:02:14 +0500

    additional template

---
+++ wp-content/themes/Divi-child/template/findus.php
@@ -0,0 +1,11 @@
+<?php 
+/* Template Name: Custom Find Us */ 
+
+get_header();
+echo the_title();
+?>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3619.584947966176!2d67.03197391510594!3d24.878021484045654!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3eb33e4340000001%3A0xe1b798a8bc2cf067!2sFatimiyah%20Hospital%2C%20Karachi!5e0!3m2!1sen!2s!4v1581926088806!5m2!1sen!2s" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+<?php
+
+get_footer();
+?>
